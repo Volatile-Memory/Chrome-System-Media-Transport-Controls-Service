6c6bb35df08c1c3c168898d66105c876ff252963
Computr0n <Vlad.Kuksenko@gmail.com>
2018-01-15 11:08:37 -0600

    post-build attach script/step

---
+++ smtc/attachToSmtc.ps1
@@ -0,0 +1,13 @@
+﻿function Debug-ProcessVS([int] $processId)
+{
+    $vsProcess = Get-Process devenv | Select-Object -First 1
+    if (!$vsProcess) {throw "Visual Studio is not running"}
+    $vsMajorVersion = $vsProcess.FileVersion -replace '^(\d+).*', '$1'
+    $dte = [System.Runtime.InteropServices.Marshal]::GetActiveObject("VisualStudio.DTE.$vsMajorVersion.0")
+    $debugee = $dte.Debugger.LocalProcesses | ? {$_.ProcessID -eq $processId}
+    if (!$debugee) {throw "Process with ID $processId does not exist."}
+    $debugee.Attach()
+}
+
+sleep -Seconds 3
+Debug-ProcessVS((Get-Process -Name smtc)[0].Id)
